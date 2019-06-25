fec3845a2259d8922142456f66f28eb9d993f4af
Adam Brown <adamarthurryan@gmail.com>
2019-06-25 13:14:07 -0400

    initial checkin

---
+++ app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { AppComponent }  from './app.component';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    SignupFormComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
+
+
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
